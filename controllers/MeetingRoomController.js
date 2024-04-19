eb834fac3001a2407daa49a8a6327015153d9eaa
ahmedmlaouhia <ahmedmlaouhia6@gmail.com>
2024-04-20 00:10:08 +0100

    almost meeting room done

--- controllers/MeetingRoomController.js
+++ controllers/MeetingRoomController.js
@@ -1,84 +1,82 @@
-const MeetingRoom = require('../models/meetingRoom');
+const MeetingRoom = require("../models/meetingRoom")
 
 // Get all meeting rooms
 exports.getAllMeetingRooms = async (req, res) => {
   try {
-    const meetingRooms = await MeetingRoom.find();
-    res.status(200).json(meetingRooms);
+    const meetingRooms = await MeetingRoom.find()
+    res.render("all-rooms", { meetingRooms: meetingRooms })
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Failed to fetch meeting rooms' });
+    console.error(error)
+    res.render("all-rooms", { error: "Failed to fetch meeting rooms" })
   }
-};
+}
 
 // Create a new meeting room
-
 exports.createMeetingRoom = async (req, res) => {
   try {
-    const { name, capacity , equipment } = req.body;
+    const { name, capacity, equipment } = req.body
     //check if room already exists
-    const existingRoom = await MeetingRoom.findOne({name});
-    
+    const existingRoom = await MeetingRoom.findOne({ name })
+
     if (existingRoom) {
-      return res.status(409).json({ error: 'Room already exists' });
+      return res.render("create-room", { error: "Room already exists" })
     }
 
-    
-    const meetingRoom = new MeetingRoom({ name, capacity, equipment });
-    await meetingRoom.save();
-    res.status(201).json(meetingRoom);
+    const meetingRoom = new MeetingRoom({ name, capacity, equipment })
+    await meetingRoom.save()
+    res.redirect("/meeting-room/all")
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Failed to create meeting room' });
+    console.error(error)
+    res.render("create-room", { error: "Failed to create meeting room" })
   }
-};
+}
 
 // Get a single meeting room by ID
 exports.getMeetingRoomById = async (req, res) => {
   try {
-    const { id } = req.params;
-    const meetingRoom = await MeetingRoom.findById(id);
+    const { id } = req.params
+    const meetingRoom = await MeetingRoom.findById(id)
     if (!meetingRoom) {
-      return res.status(404).json({ error: 'Meeting room not found' });
+      return res.render("meeting-room", { error: "Meeting room not found" })
     }
-    res.status(200).json(meetingRoom);
+    res.render("meeting-room", { meetingRoom: meetingRoom })
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Failed to fetch meeting room' });
+    console.error(error)
+    res.render("meeting-room", { error: "Failed to fetch meeting room" })
   }
-};
+}
 
 // Update a meeting room by ID
 exports.updateMeetingRoomById = async (req, res) => {
   try {
-    const { id } = req.params;
-    const { name, capacity } = req.body;
+    const { id } = req.params
+    const { name, capacity } = req.body
     const updatedMeetingRoom = await MeetingRoom.findByIdAndUpdate(
       id,
       { name, capacity },
       { new: true }
-    );
+    )
     if (!updatedMeetingRoom) {
-      return res.status(404).json({ error: 'Meeting room not found' });
+      return res.render("edit-room", { error: "Meeting room not found" })
     }
-    res.status(200).json(updatedMeetingRoom);
+    res.redirect("/meeting-room/all")
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Failed to update meeting room' });
+    console.error(error)
+    res.render("edit-room", { error: "Failed to update meeting room" })
   }
-};
+}
 
 // Delete a meeting room by ID
 exports.deleteMeetingRoomById = async (req, res) => {
   try {
-    const { id } = req.params;
-    const deletedMeetingRoom = await MeetingRoom.findByIdAndDelete(id);
+    const { id } = req.params
+    const deletedMeetingRoom = await MeetingRoom.findByIdAndDelete(id)
     if (!deletedMeetingRoom) {
-      return res.status(404).json({ error: 'Meeting room not found' });
+      return res.render("all-rooms", { error: "Meeting room not found" })
     }
-    res.status(200).json({ message: 'Meeting room deleted successfully' });
+    res.redirect("/meeting-room/all")
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Failed to delete meeting room' });
+    console.error(error)
+    res.render("all-rooms", { error: "Failed to delete meeting room" })
   }
-};
+}
